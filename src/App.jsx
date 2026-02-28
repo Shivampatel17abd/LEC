93125c584b7921b565ae1f76e70ab1abd4b87a52
SaiSmruti <saismrutiranjan1810@gmail.com>
2026-02-28 18:00:50 +0530

    config project

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import "./index.css"
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold text-white mb-4">
+          Tailwind + React 🚀
+        </h1>
+
+        <p className="text-gray-300 font-dm">
+          Your setup is working perfectly!
+        </p>
+
+        <button className="mt-6 px-6 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded-lg transition">
+          Click Me
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default App
